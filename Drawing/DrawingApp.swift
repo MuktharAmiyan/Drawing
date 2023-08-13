4dd52e009d86e58308cba66575e15fd8b9acad1a
MuktharAmiyan <amiyanmukthar@gmail.com>
2023-08-13 19:10:15 +0530

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Mukthar Amiyan on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
